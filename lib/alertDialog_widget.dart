aa31b187e4c6136b67c16cee3c154008dc2ad7ae
Mubassir Hayat <mubassirhayat@gmail.com>
2022-01-23 17:18:11 +0500

    Login screen UI.

---
+++ lib/alertDialog_widget.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class BlurryDialog extends StatelessWidget {
+  String title;
+  String content;
+
+  BlurryDialog(this.title, this.content);
+  TextStyle textStyle = TextStyle(color: Colors.black);
+
+  @override
+  Widget build(BuildContext context) {
+    return BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
+        child: AlertDialog(
+          title: new Text(
+            title,
+            style: textStyle,
+          ),
+          content: new Text(
+            content,
+            style: textStyle,
+          ),
+          actions: <Widget>[
+            new MaterialButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true).pop('dialog');
+              },
+            ),
+          ],
+        ));
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:hcais/alertDialog_widget.dart';
+import 'package:hcais/home.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+    onPrimary: Colors.lightGreenAccent,
+    //primary: Colors.lightGreenAccent,
+    //minimumSize: Size(88, 36),
+    padding: EdgeInsets.all(12),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(24)),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: 'dr.john.doe@pims.org.pk',
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: 'some password',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton(
+        style: raisedButtonStyle,
+        onPressed: () {
+          Navigator.of(context).pushNamed(HomePage.tag);
+        },
+        child: Text(
+          "Log In",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+
+    final forgotLabel = TextButton(
+      onPressed: () {
+        _showDialog(context);
+      },
+      child: Text(
+        "Forgot password?",
+        style: TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: 48.0),
+            email,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 24.0),
+            loginButton,
+            forgotLabel,
+          ],
+        ),
+      ),
+    );
+  }
+
+  _showDialog(BuildContext context) {
+    VoidCallback continueCallBack = () => {
+          Navigator.of(context).pop(),
+          // code on continue comes here
+        };
+    BlurryDialog alert = BlurryDialog("Forgot Password?",
+        "PLease contact your hospital administrator to reset your password.");
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+}
