1011cdd52d2b0002fe3a681dc80b17ba5fcc82b9
Mubassir Hayat <mubassirhayat@gmail.com>
2022-01-24 14:17:31 +0500

    cool stepper added

--- lib/ssi_form.dart
+++ lib/ssi_form.dart
@@ -1,42 +1,203 @@
+import 'package:cool_stepper/cool_stepper.dart';
 import 'package:flutter/material.dart';
 
-class SsiFormPage extends StatelessWidget {
+class SsiFormPage extends StatefulWidget {
+  SsiFormPage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
   static String tag = 'ssi-form-page';
 
   @override
-  Widget build(BuildContext context) {
-    final formName = Padding(
-      padding: EdgeInsets.fromLTRB(10.0, 45.0, 10.0, 15.0),
-      child: Text(
-        'Surgical Site Inspections (SSIs) Form',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 28.0,
-          color: Colors.white,
-        ),
-      ),
-    );
+  _SsiFormPageState createState() => _SsiFormPageState();
+}
 
-    final body = Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(28.0),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.blue,
-            Colors.lightBlueAccent,
-          ],
+class _SsiFormPageState extends State<SsiFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? selectedRole = 'Writer';
+  final TextEditingController _nameCtrl = TextEditingController();
+  final TextEditingController _emailCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final steps = [
+      CoolStep(
+        title: 'Hospital Information',
+        subtitle: 'Please fill the hospital information below',
+        content: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              _buildTextField(
+                labelText: 'Name',
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Name is required';
+                  }
+                  return null;
+                },
+                controller: _nameCtrl,
+              ),
+              _buildTextField(
+                labelText: 'Email Address',
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Email address is required';
+                  }
+                  return null;
+                },
+                controller: _emailCtrl,
+              ),
+            ],
+          ),
         ),
+        validation: () {
+          if (!_formKey.currentState!.validate()) {
+            return 'Fill form correctly';
+          }
+          return null;
+        },
       ),
-      child: Column(
-        children: <Widget>[
-          formName,
-        ],
+      CoolStep(
+        title: 'Select your role',
+        subtitle: 'Choose a role that better defines you',
+        content: Container(
+          child: Row(
+            children: <Widget>[
+              _buildSelector(
+                context: context,
+                name: 'Writer',
+              ),
+              SizedBox(width: 5.0),
+              _buildSelector(
+                context: context,
+                name: 'Editor',
+              ),
+            ],
+          ),
+        ),
+        validation: () {
+          return null;
+        },
+      ),
+    ];
+
+    final stepper = CoolStepper(
+      showErrorSnackbar: false,
+      onCompleted: () {
+        print('Steps completed!');
+      },
+      steps: steps,
+      config: CoolStepperConfig(
+        backText: 'PREV',
       ),
     );
 
     return Scaffold(
-      body: body,
+      appBar: AppBar(
+        title: Text(widget.title?.toUpperCase() ?? 'SSI FORM'),
+      ),
+      body: Container(
+        child: stepper,
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    String? labelText,
+    FormFieldValidator<String>? validator,
+    TextEditingController? controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: labelText,
+        ),
+        validator: validator,
+        controller: controller,
+      ),
+    );
+  }
+
+  Widget _buildSelector({
+    BuildContext? context,
+    required String name,
+  }) {
+    final isActive = name == selectedRole;
+
+    return Expanded(
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+        decoration: BoxDecoration(
+          color: isActive ? Theme.of(context!).primaryColor : null,
+          border: Border.all(
+            width: 0,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: RadioListTile(
+          value: name,
+          activeColor: Colors.white,
+          groupValue: selectedRole,
+          onChanged: (String? v) {
+            setState(() {
+              selectedRole = v;
+            });
+          },
+          title: Text(
+            name,
+            style: TextStyle(
+              color: isActive ? Colors.white : null,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
+// class _SsiFormPageState extends State<SsiFormPage> {
+//   final _formKey = GlobalKey<FormState>();
+//   String? selectedRole = 'Writer';
+//   final TextEditingController _nameCtrl = TextEditingController();
+//   final TextEditingController _emailCtrl = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final formName = Padding(
+//       padding: EdgeInsets.fromLTRB(10.0, 45.0, 10.0, 15.0),
+//       child: Text(
+//         'Surgical Site Inspections (SSIs) Form',
+//         textAlign: TextAlign.center,
+//         style: TextStyle(
+//           fontSize: 28.0,
+//           color: Colors.white,
+//         ),
+//       ),
+//     );
+
+//     final body = Container(
+//       width: MediaQuery.of(context).size.width,
+//       padding: EdgeInsets.all(28.0),
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: [
+//             Colors.blue,
+//             Colors.lightBlueAccent,
+//           ],
+//         ),
+//       ),
+//       child: Column(
+//         children: <Widget>[
+//           formName,
+//         ],
+//       ),
+//     );
+
+//     return Scaffold(
+//       body: body,
+//     );
+//   }
+// }
