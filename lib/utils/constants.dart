b7c92d770d1df5ed8a654fe089b0e23b65c754e0
sohail0992 <msohail.se@gmail.com>
2023-04-27 17:12:54 +0500

    dev url

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
-  static const BASE_URL = 'http://localhost:3000'; //prod
+  static const dev = true;
+  static const BASE_URL =
+      dev ? 'http://18.141.220.82:8080' : 'http://18.141.220.82:3000'; //prod
   static const API_KEY = ''; //prod
 }
