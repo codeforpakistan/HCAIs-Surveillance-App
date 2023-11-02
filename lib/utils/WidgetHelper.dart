5e8d9bdc704d4b2d05ac8bb1db4294eaee431c96
sohail0992 <msohail.se@gmail.com>
2023-11-02 13:01:44 +0500

    label for multi select

--- lib/utils/WidgetHelper.dart
+++ lib/utils/WidgetHelper.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:hcais/components/alertDialog_widget.dart';
 
 class WidgetHelper {
-  static Column buildColumn(String label, bool isRequired) {
+  static Column buildColumn(String label, bool isRequired, context,
+      [String helperText = '']) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      RichText(
-        textAlign: TextAlign.center,
-        text: TextSpan(children: <TextSpan>[
-          TextSpan(
-              text: label.toString(), style: TextStyle(color: Colors.black)),
-          TextSpan(
-              text: isRequired ? " *" : "",
-              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
-        ]),
-        softWrap: true, // Allow text
+      Row(
+        children: [
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(children: <TextSpan>[
+              TextSpan(
+                  text: label.toString(),
+                  style: TextStyle(color: Colors.black)),
+              TextSpan(
+                  text: isRequired ? " *" : "",
+                  style: TextStyle(
+                      color: Colors.red, fontWeight: FontWeight.bold)),
+            ]),
+            softWrap: true, // Allow text
+          ),
+          Visibility(
+            visible: helperText != '',
+            child: SizedBox(width: 8), // Add some space between Icon and Text
+          ),
+          GestureDetector(
+            onTap: () {
+              // Call your function here
+              _showDialog(context, helperText);
+            },
+            child: Visibility(
+              visible: helperText != '',
+              child: Icon(
+                Icons.info_outline, // Replace with the desired icon
+              ),
+            ),
+          ),
+        ],
       ),
       SizedBox(height: 4)
     ]);
+  }
+
+  static void _showDialog(BuildContext context, String helpLabelText) {
+    List<Widget> buttons = [];
+    buttons.add(new MaterialButton(
+      child: Text("Ok"),
+      onPressed: () {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+      },
+    ));
+    BlurryDialog alert = BlurryDialog('Information', helpLabelText, buttons);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 }
